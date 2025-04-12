7b72fca6c00d267663f92df3c74fd090a04cf58c
Stephany Suarez <Stephanyrsr@gmail.com>
2025-04-12 13:06:16 -0300

    agregue el  fetch y organicé un poco la arquitectura

--- src/Services/getProducts.js
+++ src/Services/getProducts.js
@@ -1,1 +1,12 @@
 /*fetching apí un archivo por endpoint*/
+export async function getProducts(){
+    
+    try {
+            const response = await fetch('https://fakestoreapi.com/products');
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error('Error al obtener los productos:', error);
+            return [];
+        }
+}
