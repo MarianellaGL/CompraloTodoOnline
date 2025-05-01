1f3b4fcc4cd3b785f1216dbb76a00a08938cd4c9
marianellagl <Marianella.gomezluna@gmail.com>
2025-05-01 16:41:39 -0300

    fixing app.js

--- src/Services/getProductsFromAPI.js
+++ src/Services/getProductsFromAPI.js
@@ -1,12 +1,11 @@
 /*fetching apí un archivo por endpoint*/
-export async function getProductsFromAPI(){
-    
-    try {
-            const response = await fetch('https://fakestoreapi.com/products');
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            console.error('Error al obtener los productos:', error);
-            return [];
-        }
-}
+export const getProductsFromAPI = async () => {
+  try {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error al obtener los productos:", error);
+    return [];
+  }
+};
