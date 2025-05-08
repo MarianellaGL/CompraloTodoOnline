ee9d9225c4482b2494efed8fe5af6ed665fdbf9e
juanpidev92 <juan.mancebo@istea.com.ar>
2025-05-08 18:18:19 +0200

    Update app.js
    Para utilizar el spinner

--- src/App/app.js
+++ src/App/app.js
@@ -1,5 +1,15 @@
-/*global logic*/
-import { getProductsFromAPI } from "../Services/getProductsFromAPI"  
+//para el spinner
+import { Spinner } from "../Components/Spinner.js";
 
-const productsContainer = document.getElementById('products')
-console.log(productsContainer)
+let products = [];
+
+const loadingDiv = document.getElementById('products');
+loadingDiv.innerHTML = '<div class="text-center p-5"><div class="spinner-border text-primary" role="status"></div></div>';
+
+products = await getProductsFromAPI();
+
+if (products.length === 0) {
+  loadingDiv.innerHTML = '<p class="text-danger">No se encontraron productos.</p>';
+} else {
+  renderCards(products, "Productos");
+}
