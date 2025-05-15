286206117d0d0470377bf4648bfade50566aaa49
juanpidev92 <juan.mancebo@istea.com.ar>
2025-05-15 19:54:20 +0200

    Update showToast.js
    File nuevo

--- src/Utils/showToast.js
+++ src/Utils/showToast.js
@@ -1,1 +1,15 @@
+// /src/Utils/showToast.js
 
+export function showToast(message, color = "#00b09b") {
+  Toastify({
+    text: message,
+    duration: 3000,
+    close: true,
+    gravity: "top",
+    position: "right",
+    style: {
+      background: `linear-gradient(to right, ${color}, ${color})`
+    },
+    stopOnFocus: true,
+  }).showToast();
+}
